2f9f113169cd2756552ef5ad49df0f8e0121bfc6
Harald von Waldow <harald.vonwaldow@eawag.ch>
2019-09-02 16:53:04 +0200

    added startup-script to set display/theme parameters in DB

---
+++ ckan/docker-entrypoint.d/set_display.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+psql $CKAN_SQLALCHEMY_URL -c "UPDATE system_info SET value='/base/images/combined_logo1_fontexo_open.svg' WHERE key='ckan.site_logo';"
+psql $CKAN_SQLALCHEMY_URL -c "UPDATE system_info SET value=3 WHERE key='ckan.homepage_style';"                                                                
+psql $CKAN_SQLALCHEMY_URL -c "UPDATE system_info SET value='ERIC-open' WHERE key='ckan.site_title';"
